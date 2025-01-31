f1f97bb34f087e66f6046ce36d4da1d82081b086
Andriy Pertsov <andriy.pertsov@lseg.com>
2025-01-31 17:37:06 +0200

    UOF using added

---
+++ CompanyApp.Utilities/Program.cs
@@ -0,0 +1,22 @@
+﻿using CompanyApp.DAL.Data;
+using CompanyApp.DAL.Repositories;
+using CompanyApp.Domain.Entities;
+
+namespace CompanyApp.Utilities
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            using (var unitOfWork = new UnitOfWork(new ApplicationDbContext()))
+            {
+                // Example operation - Adding a new customer
+                var newCustomer = new Customer { Name = "John Doe", Email = "john@doe.com" };
+                unitOfWork.Customers.AddAsync(newCustomer);
+
+                await unitOfWork.CommitAsync(); // Commit transaction
+            }
+        }
+    }
+}
